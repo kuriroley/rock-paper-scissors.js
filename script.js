bd6285938b1b131ef1bd02156c24ba576daf1e10
kuriroley <christian.rodriguez@tutanota.com>
2023-05-24 17:05:22 +0900

    added loop and round counter to the game

--- script.js
+++ script.js
@@ -2,44 +2,58 @@
  - the computer is able to return values, but i cannot set a way to perform a game? 
 
 */
-let gameTurns = 1;
+let gameRounds = 1;
+function game(){
 
-
-function playRound (playerSelection, computerSelection){   //how can I make this work?
+    function playRound (playerSelection, computerSelection){   //the values work! 
     
-    let gameResult = ''
-    if (playerSelection === computerSelection){
-        gameResult = 'tie // あいこ';
-    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-        gameResult = 'paper wins! // パー勝った'
-    } else if (playerSelection === 'rock' && computerSelection === 'scissor'){
-        gameResult = 'rock wins! // グ－勝った'
-    } else if (playerSelection === 'scissor' && computerSelection === 'paper'){
-        gameResult = 'scissor wins! // チョキ勝った'
-    } else {
-        gameResult = 'Computer wins! // パソコン勝った'
+        let gameResult = ''
+        
+        if (playerSelection === computerSelection){
+                gameResult = 'tie // あいこ';
+        } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+                gameResult = 'paper wins! // パー勝った'
+        } else if (playerSelection === 'rock' && computerSelection === 'scissor'){
+                gameResult = 'rock wins! // グ－勝った'
+        } else if (playerSelection === 'scissor' && computerSelection === 'paper'){
+                gameResult = 'scissor wins! // チョキ勝った'
+        } else {
+                gameResult = 'Computer wins! // パソコン勝った'
+        }
+        return gameResult;
     }
 
-    return gameResult;
-}
-function getComputerChoice(){       // it works, the value is randomly returned
-    let result = '';
-    let choice = Math.floor(Math.random() * 3 ) + 1 ;
-    if (choice === 1 ){
-        result = 'Rock';
-    } else if (choice === 2 ){
-        result = 'Paper';
-    } else if (choice === 3 ){
-        result = 'Scissor';
-    } else {
-        ' '
+    function getComputerChoice(){       // it works, the value is randomly returned
+        let result = '';
+        let choice = Math.floor(Math.random() * 3 ) + 1 ;
+        if (choice === 1 ){
+            result = 'Rock';
+        } else if (choice === 2 ){
+            result = 'Paper';
+        } else if (choice === 3 ){
+            result = 'Scissor';
+        } else {
+            ' '
+        }
+        return result;
     }
-    return result;
+    const playerSelection = (prompt('rock, paper or scissors ??? ').toLowerCase());
+    const computerSelection = (getComputerChoice().toLowerCase());
+    console.log(computerSelection)
+    console.log('Rounds: ' + gameRounds)
+    console.log(playRound(playerSelection, computerSelection));
+    gameRounds++;
+
 }
-const playerSelection = (prompt('rock, paper or scissors ??? ').toLowerCase());
-const computerSelection = (getComputerChoice().toLowerCase());
-console.log(computerSelection)
-console.log(playRound(playerSelection, computerSelection));
+
+do{
+    game()
+}while (gameRounds <= 5)
+
+
+
+
+
 
 
 
