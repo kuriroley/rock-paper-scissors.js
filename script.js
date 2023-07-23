88338d1f85efc8c96f1fbe481922550c77c2ebb1
kuriroley <christian.rodriguez@tutanota.com>
2023-07-23 18:45:52 +0900

    new values

--- script.js
+++ script.js
@@ -1,56 +1,131 @@
-/* 
- - the computer is able to return values, but i cannot set a way to perform a game? 
+let roundTracker = 1;
+let computerScore = 0;
+let playerScore = 0;
+let playerAnswer = '';
+let computerAnswer = getComputerAnswer();
 
-*/
-let gameRounds = 1;
-function game(){
+//dom documents
+const scoreContainer = document.querySelector('#score-container');
+const paraPlayerScore = document.createElement('p');
+const paraComputerScore = document.createElement('p');
+const paraGameResult = document.createElement('p');
+const paraRoundTracker = document.createElement('p');
+const winnerContainer = document.querySelector('#winner-container');
+const winnerIs = document.createElement('h1');
+const paraPlayerAnswer = document.createElement('p');
+const paraComputerAnswer = document.createElement('p');
+//dom buttons
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorButton = document.querySelector('#scissor-button');
 
-    function playRound (playerSelection, computerSelection){   //the values work! 
-    
-        let gameResult = ''
+//dom buttons events
+rockButton.addEventListener('click', () =>{
+    playerAnswer = 'rock';
+    computerAnswer = getComputerAnswer();
+    gameRound(playerAnswer, computerAnswer);
+    gameData();
+    if(playerScore === 5 || computerScore === 5){
+        declareWinner();
+     }
+
+})
+paperButton.addEventListener('click', () => {
+    playerAnswer = 'paper';
+    computerAnswer = getComputerAnswer();
+    gameRound(playerAnswer, computerAnswer);
+    gameData();
+    if(playerScore === 5 || computerScore === 5){
+        declareWinner();
+     }
+})
+scissorButton.addEventListener('click', () => {
+    playerAnswer = 'scissor';
+    computerAnswer = getComputerAnswer();
+    gameRound(playerAnswer, computerAnswer);
+    gameData();
+    if(playerScore === 5 || computerScore === 5){
+        declareWinner();
+     }
+})
+
+//get computer answer funtion
+function getComputerAnswer(){
+    const choices = ['rock', 'paper', 'scissor'];
+    const random = Math.floor(Math.random() * choices.length);
+    let computerOption = '';
+    if (random === 0){
+        computerOption = 'rock';
+    } else if (random === 1){
+        computerOption = 'paper';
+    } else if (random === 2){
+        computerOption = 'scissor';
+    }
+    return computerOption;
+}
+//main game function that sets a round
+function gameRound(playerAnswer, computerAnswer){
+    let gameResult = '';
+    if (playerAnswer === computerAnswer){
+        gameResult = 'tie';
+    } else if (playerAnswer === 'rock' && computerAnswer === 'scissor'){
+        gameResult = 'rock wins';
+        playerScore++;
+    } else if (playerAnswer === 'scissor' && computerAnswer === 'paper'){
+        gameResult = 'player scissor wins';   
+        playerScore++;
+    } else if (playerAnswer === 'paper' && computerAnswer === 'rock'){
+        gameResult = 'player paper wins!';
+        playerScore++;
+    } else {
+        gameResult = `Computer's ${computerAnswer} win!`;
+        computerScore++;
+    }
+    return gameResult;
+}
+// a function to declare the end of the game
+function declareWinner(){
+    if(playerScore == computerScore){
+        winnerContainer.appendChild(winnerIs);
+        winnerIs.textContent = 'IS A TIE!';
+    }
+    else if(playerScore > computerScore){
+        winnerContainer.appendChild(winnerIs);
+        winnerIs.textContent = 'YOU WIN!';
+    } else{
+        winnerContainer.appendChild(winnerIs);
+        winnerIs.textContent = 'COMPUTER WINS! ';
         
-        if (playerSelection === computerSelection){
-                gameResult = 'tie // あいこ';
-        } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-                gameResult = 'paper wins! // パー勝った'
-        } else if (playerSelection === 'rock'&& computerSelection === 'scissor'){
-                gameResult = 'rock wins! // グ－勝った'
-        } else if (playerSelection === 'scissor' && computerSelection === 'paper'){
-                gameResult = 'scissor wins! // チョキ勝った'
-        } else {
-                gameResult = 'Computer wins! // パソコン勝った'
-        }
-        return gameResult;
     }
+}
+function gameData(){
+scoreContainer.appendChild(paraPlayerAnswer);
+paraPlayerAnswer.textContent = `Your choice was: ` + playerAnswer;
+scoreContainer.appendChild(paraComputerAnswer);
+paraComputerAnswer.textContent = `Computer's answer was: ` + computerAnswer;
 
-    function getComputerChoice(){       // it works, the value is randomly returned
-        let result = '';
-        let choice = Math.floor(Math.random() * 3 ) + 1 ;
-        if (choice === 1 ){
-            result = 'Rock';
-        } else if (choice === 2 ){
-            result = 'Paper';
-        } else if (choice === 3 ){
-            result = 'Scissor';
-        } else {
-            ' '
-        }
-        return result;
-    }
-    const playerSelection = (prompt('じゃんけん! // rock, paper or scissors ??? ').toLowerCase());
-    const computerSelection = (getComputerChoice().toLowerCase());
-    console.log(computerSelection)
-    console.log('ラウンド/Rounds: ' + gameRounds)
-    console.log(playRound(playerSelection, computerSelection));
-    gameRounds++;
+scoreContainer.appendChild(paraRoundTracker);
+paraRoundTracker.textContent = `Round: ${roundTracker++}`;
+if (roundTracker > 5){
+    paraRoundTracker.textContent = `NO MORE ROUNDS!`;
+    return false;
+};
+scoreContainer.appendChild(paraPlayerScore);
+paraPlayerScore.textContent = `Player score is: ${playerScore}`;
+scoreContainer.appendChild(paraComputerScore);
+paraComputerScore.textContent = `Computer score is: ${computerScore}`;
 
 }
-do{
-    game()
-}while (gameRounds <= 5)
 
 
 
+// console.log(`Computer Selection: ${computerSelection}`);
+//     console.log('ラウンド/Rounds: ' + gameRounds)
+//     // console.log(playRound(playerSelection, computerSelection));
+//     console.log(`Computer Score:  ${computerScore}`);
+//     console.log(`User Score: ${userScore}`);    
+
+//     gameRounds++;
 
 
 
